c9d4fcd1e8b9f9db3d66bba49f4ddf03e637a6e5
Clara <ershovaclara@yandex.ru>
2023-10-13 19:52:30 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return 'web-сервер на flask'
+    return"""
+<!doctype html>
+<html>
+    <head>
+        <title>Боков Антон Константинович, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Антон Боков, ФБИ-12, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+""" 
+
