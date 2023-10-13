8c0a7428d913a86f2bc0a694291020d1bec61dd0
Clara <ershovaclara@yandex.ru>
2023-10-13 23:54:15 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -179,3 +179,8 @@ def example():
     ]   
 
     return render_template('example.html', name=name, group=group, cours=cours,number=number,fruits=fruits, books=books)
+
+
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
