e344ed8cec04c5057ad0435859bca577bac0b3e9
Clara <ershovaclara@yandex.ru>
2023-11-24 00:43:40 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -26,3 +26,33 @@ def login():
         error = 'Не введен пароль!'
     
     return render_template('login.html', error=error,username=username,password=password)
+
+
+@lab4.route('/lab4/fridge', methods=['GET','POST'])
+def fridge():
+    if request.method == 'GET':
+        return render_template('fridge.html')
+   
+    temp = request.form.get('temp')
+    error=''
+
+    if temp == '':
+        error = 'Ошибка: не задана температура'
+    else:
+        if temp:
+            temp=int(temp)
+            if (temp>-13) and 0>temp:
+                if (temp>-13) and (-8>temp):
+                    snow = '❄️❄️❄️'
+                elif (temp>-9) and (-4>temp):
+                    snow = '❄️❄️'
+                elif (temp>-5) and (0>temp):
+                    snow = '❄️'
+                return render_template('success_fridge.html',temp=temp,snow=snow)
+
+            if temp <-12:
+                error = 'Не удалось установить температуру: слишком низкое значение'
+            if temp >-1:
+                error = 'Не удалось установить температуру: слишком высокое значение'
+
+    return render_template('fridge.html',temp=temp,error=error)
