24794b6d6263839397bfa362ad5f0d7d2136d7fb
Clara <ershovaclara@yandex.ru>
2023-12-22 05:24:20 +0700

    Главная страница

--- lab5.py
+++ lab5.py
@@ -6,46 +6,24 @@ lab5 = Blueprint('lab5',__name__)
 
 
 def dbConnect():
-    # Прописываем параметры подключения к БД
     conn = psycopg2.connect(
         host="127.0.0.1",
-        database="knowledge_base_for_ersh_trub",
-        user="ersh_trub_knowledge_base",
-        password="ershtrub123")
+        database="knowledge_base_for_bokov",
+        user="bokov_knowledge_base",
+        password="bokov_user")
     return conn
 
 
 def dbClose(cursor,connection):
-    # Закрываем курсор и соединение 
     cursor.close()
     connection.close()
 
 
 @lab5.route('/lab5/')
 def main():
-    conn = psycopg2.connect(
-        host="127.0.0.1",
-        database="knowledge_base_for_bokov",
-        user="bokov_knowledge_base",
-        password="bokov_user")
-    
-    cur=conn.cursor()
-
-    cur.execute ("SELECT * FROM users;")
-
-    result = cur.fetchall()
-
-
-    cursor.close()
-    connection.close()
-
-    print(result)
-    return 'go to console'
-
-
     username=session.get("username")
     if username =='':
         visibleUser='Anon'
     else:
         visibleUser=username
-    return render_template('5_main.html',username=visibleUser)
+    return render_template('main.html',username=visibleUser)
