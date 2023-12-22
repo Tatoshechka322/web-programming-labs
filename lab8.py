d185020f5c14296d3eddbfaebacf843351abc375
Clara <ershovaclara@yandex.ru>
2023-12-23 03:38:36 +0700

    Последние правки

--- lab8.py
+++ lab8.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, request, abort, jsonify
 import datetime
-
 lab8=Blueprint('lab8',__name__)
 
 @lab8.route('/lab8/')
@@ -14,15 +13,16 @@ courses = [
 ]
 
 @lab8.route('/lab8/api/courses/', methods=['GET'])
-def get_courses2(course_num):
-    return courses[course_num]
+def get_courses2():
+    return jsonify (courses)
 
 @lab8.route('/lab8/api/courses/<int:course_num>', methods=['GET'])
 def get_courses3(course_num):
     if 0<= course_num < len(courses)-1:
+        return courses[course_num]
+    else:
         abort(404, "Course not found")
-    return courses[course_num]
-
+    
 @lab8.route('/lab8/api/courses/<int:course_num>',methods=['DELETE'])
 def del_courses(course_num):
     if 0<= course_num < len(courses)-1:
@@ -30,19 +30,22 @@ def del_courses(course_num):
         return '', 204
     else:
         abort(404, "Course not found")
+    
 
 @lab8.route('/lab8/api/courses/<int:course_num>',methods=['PUT'])
 def put_courses(course_num):
-    if course_num < 0 or course_num > len(courses)-1:
-        abort(404, "Course not found")
-    course = request.get_json()
     courses[course_num] = course
-    return courses[course_num]
-        
+    course = request.get_json()
+
+    if 0<=  course_num < len(courses)-1:
+        return courses[course_num]
+    else:
+        abort(404, "Course not found")
+    
 @lab8.route('/lab8/api/courses/', methods=['POST'])
 def add_course():
     course = request.get_json()
     courses.append(course)
+    course["date_"] = datetime.datetime.now().strftime('%Y-%m-%d')
     return {"num": len(courses)-1}
-
     
